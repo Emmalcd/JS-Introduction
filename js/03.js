c175d97e68734a5a4c5bca3ea44b13c34c8338b8
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 12:23:51 -0600

    Update script reference and add new JavaScript file with const examples

---
+++ js/03.js
@@ -0,0 +1,20 @@
+// Variables with const
+
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+const disponible=true; // No podemos iniciar la variable sin valor
+
+// producto = true; // Reasignando el valor de la variable
+// disponible = true; //No podemos reasignar un valor a una constante
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Iniciar multiples variables y asignarles valor
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // camelcase
+const NombreProducto = 'Monitor HD'; // pascal case
+const nombreproducto = 'Monitor HD'; // lowercase
+
+console.log(producto); // Mostrar en consola
