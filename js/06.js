b91b784ea85cfc618234e7049df3769c0281948d
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 13:59:02 -0600

    Add new JavaScript file with Math object examples

---
+++ js/06.js
@@ -0,0 +1,16 @@
+// Math Object
+
+let resultado;
+
+resultado=Math.PI;
+resultado=Math.round(3.99);
+resultado=Math.ceil(2.1);
+resultado=Math.floor(2.9);
+resultado=Math.sqrt(144);  
+resultado=Math.abs(-200);
+resultado=Math.min(3,5,1,12,9);
+resultado=Math.max(3,5,1,12,9);
+resultado=Math.random();
+resultado=Math.floor(Math.random()*30);
+
+console.log(resultado);
