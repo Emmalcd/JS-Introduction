586f93c700c4c0a684e643c58202510135c6efbb
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 12:19:24 -0600

    Update script reference and add new JavaScript file with let examples

---
+++ js/02.js
@@ -0,0 +1,20 @@
+// Variables with let
+
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+let disponible; // Iniciamos la variable pero sin valor
+
+// producto = true; // Reasignando el valor de la variable
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Iniciar multiples variables y asignarles valor
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // camelcase
+let NombreProducto = 'Monitor HD'; // pascal case
+let nombreproducto = 'Monitor HD'; // lowercase
+
+console.log(producto); // Mostrar en consola
