73f01575bbf95f1adcf57e1e87fbc4b465026e2d
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 13:50:11 -0600

    Update script references and add new JavaScript file with number examples

---
+++ js/05.js
@@ -0,0 +1,16 @@
+// Numbers
+
+const number1= 100;
+const number2= 200;
+const number3= "300";
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+
+console.log(number1+number2);
+console.log(number1+number3 + " (Esto es una concatenación) de " + number1 + " y " + number3);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
