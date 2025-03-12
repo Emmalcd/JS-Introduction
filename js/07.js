daede4d648f3097c9b5aa0498a88d1b89d96520f
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 15:04:33 -0600

    Update script reference and add new JavaScript file with operations order and increment/decrement examples

---
+++ js/07.js
@@ -0,0 +1,24 @@
+// Operations order
+let resultado = 10 + 10 * 5;
+console.log(resultado); // 60
+
+resultado = (10 + 10) * 5;
+console.log(resultado); // 100
+
+
+//Increment and decrement operators
+
+let numero = 10;
+numero++;
+console.log(numero); // 11
+console.log(++numero); // 12
+console.log(numero++); // 12
+console.log(numero); // 13
+
+numero--;
+console.log(numero); // 12
+numero+=2;
+console.log(numero); // 14
+
+numero-=10;
+console.log(numero); // 4
