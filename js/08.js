5174bd46bf3e44693ee0047773a5de14df5a6eec
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 15:11:18 -0600

    Update script reference and add new JavaScript file with string concatenation and template string examples

---
+++ js/08.js
@@ -0,0 +1,8 @@
+// concatenation
+const nombre = 'Juan';
+const email = 'correo@correo';
+
+console.log("Nombre: " + nombre + " e-mail: " + email);
+
+// template strings
+console.log(`Nombre: ${nombre} e-mail: ${email}`);
