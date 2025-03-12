316d570bd34c04f60834e8cab036db5a390365f9
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 12:15:40 -0600

    Update script reference and add initial JavaScript file with variable examples

---
+++ js/01.js
@@ -0,0 +1,19 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+var disponible; // Iniciamos la variable pero sin valor
+
+// producto = true; // Reasignando el valor de la variable
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Iniciar multiples variables y asignarles valor
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // camelcase
+var NombreProducto = 'Monitor HD'; // pascal case
+var nombreproducto = 'Monitor HD'; // lowercase
+
+console.log(producto); // Mostrar en consola
