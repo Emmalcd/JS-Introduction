8a728e26368aceef2407b05f3a1a0c8c91aa4b29
Emmalcd <joselemmanuel9@gmail.com>
2025-03-12 17:44:18 -0600

    Update script reference and add new JavaScript file with boolean examples

---
+++ js/09.js
@@ -0,0 +1,15 @@
+//Booleans
+//Booleans are true/false values and help with control flow and logic. They are used to test conditions and make decisions.
+
+const bool1 = true;
+const bool2 = false;
+
+console.log(bool1);
+console.log(bool2);
+
+const bool3 = new Boolean(true);
+console.log(bool3);
+
+console.log(typeof bool1);
+console.log(typeof bool2);
+console.log(typeof bool3);
