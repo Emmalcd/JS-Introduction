e67d5445e5d3d58109786c596b9a189d77cae31a
Emmalcd <joselemmanuel9@gmail.com>
2025-03-13 15:15:50 -0600

    Update script reference and add new JavaScript file with functions examples

---
+++ js/19.js
@@ -0,0 +1,16 @@
+function suma(number1=0, number2=0){
+    console.log(number1+number2);
+}
+
+suma(3,3);
+suma(3,9);
+suma(2,9);
+
+suma(2);
+suma();
+
+const multiplicacion = function(n1,n2){
+    console.log(n1*n2);
+}
+
+multiplicacion(3,3);
