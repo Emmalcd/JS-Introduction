f8d13d5ae496ef59a3c7757630634be6b2da0f2b
Emmalcd <joselemmanuel9@gmail.com>
2025-03-13 08:45:14 -0600

    Update script reference and add new JavaScript file with destructuring objects examples

---
+++ js/11.js
@@ -0,0 +1,18 @@
+// Variables and Objects
+
+const product = {
+    name: 'Monitor 20 "',
+    price: 300,
+    available: true,
+    id: 123
+}
+
+const name = product.name;
+const price = product.price;
+
+const { available, id } = product;
+
+console.log(name);
+console.log(price);
+console.log(available);
+console.log(id);
