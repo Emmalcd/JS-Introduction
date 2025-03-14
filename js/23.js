96c5af53d109bb426f42d675407efc615f26f7ae
Emmalcd <joselemmanuel9@gmail.com>
2025-03-13 23:25:12 -0600

    Update script reference and add new JavaScript file with switch statement examples

---
+++ js/23.js
@@ -0,0 +1,31 @@
+const dia="Domingo";
+
+switch(dia){
+    case "Lunes":
+        console.log("El día es lunes");
+        break;
+    case "Martes":
+        console.log("El día es martes");
+        break;
+    case "Miercoles":
+        console.log("El día es miercoles");
+        break;
+    case "Jueves":
+        console.log("El día es jueves");
+        break;
+    case "Viernes":
+        console.log("El día es viernes");
+        break;
+    case "Sabado":
+        console.log("El día es sabado");
+        break;
+    case "Domingo":
+        console.log("El día es domingo");
+        break;
+        
+
+    default:
+        console.log("No agregaste un día valido");
+        break;
+    
+}
